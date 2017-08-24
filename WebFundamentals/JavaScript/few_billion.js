805785f0624465735644fc093ae6751484e4a724
Sandy <beachwhale13@yahoo.com>
2017-08-24 11:51:00 -0700

    added jQuery assignments

--- WebFundamentals/JavaScript/few_billion.js
+++ WebFundamentals/JavaScript/few_billion.js
@@ -9,3 +9,18 @@ function fewBillion(days){
 fewBillion(30);
 
 //answer: 5,368,709.12
+
+//how many days to make 10,000?
+function fewBillion(days){
+  var sum = .01;
+  for (var i = 2; i<=days; i++){
+    sum = sum + sum;
+    if (sum >= 10000.00){
+      console.log(i);
+      break;
+    }
+  }
+  return sum;
+}
+
+//answer: 21 days, you have $10,485.76
--- WebFundamentals/JavaScript/range_print.js
+++ WebFundamentals/JavaScript/range_print.js
@@ -5,8 +5,23 @@ function printRange(start, end, skip){
 }
 
 
+//if user doesn't pass a skip amount, default to 1
+
 function printRange(start, end, skip){
   for (var i = start; i < end; i += (skip || 1)){
     console.log(i);
   }
 }
+
+//if user doesn't pass and end point, make it start at 0 and end at the first
+function printRange(start, end, skip){
+	if (typeof start = "number" && typeof end = "number"){
+		for (var i = start; i < end; i += (skip || 1)){
+    		console.log(i);
+		}
+  	}
+  	else {
+  		for (var i = 0; i < start; i += (skip || 1)){
+  			console.log(i);
+  		}
+  	}
